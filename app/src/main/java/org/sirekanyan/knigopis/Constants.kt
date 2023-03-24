2cc2fb954e26a977d268a316db40be5b41d67c65
sirekanian <sirekanian@pm.me>
2023-03-25 03:35:26 +0400

    Fixed static api url

--- app/src/main/java/org/sirekanyan/knigopis/Constants.kt
+++ app/src/main/java/org/sirekanyan/knigopis/Constants.kt
@@ -2,7 +2,7 @@ package org.sirekanyan.knigopis
 
 const val MAIN_API = "https://api.knigopis.com"
 const val MAIN_WWW = "https://www.knigopis.com"
-const val STATIC_API = "https://static.sirekanyan.com/img"
+const val STATIC_API = "https://sirekanian.com/kp/img"
 const val DATE_FORMAT = "yyyy-MM-dd HH:mm:ss"
 const val MIN_BOOK_PRIORITY = 1
 const val MAX_BOOK_PRIORITY = 100
