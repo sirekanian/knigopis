55b4bca47242eeb6b6de703ce6b9bff582e5efdc
sirekanian <sirekanian@pm.me>
2023-04-13 14:45:00 +0400

    Added version-checker plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("kotlin-parcelize")
+    id("org.sirekanyan.version-checker")
 }
 
 android {
@@ -84,6 +85,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test.uiautomator:uiautomator:2.2.0")
+}
+
+versionCheckerOptions {
+    "com.squareup.okhttp3:logging-interceptor" lessThan "4.0"
 }
 
 task("updateReadme") {
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,12 @@ buildscript {
     repositories {
         mavenCentral()
         google()
+        gradlePluginPortal()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
+        classpath("org.sirekanyan:version-checker:1.0.0")
     }
 }
 
